0bf7422013ff48c1effa992301ba6d37bd24112f
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-06-24 17:51:22 +0000

    feat: add ThreeDotBounce animation

---
+++ src/components/ThreeDotBounce.tsx
@@ -0,0 +1,50 @@
+import React, { SVGProps } from "react";
+
+export default function ThreeDotBounce({
+  width = 24,
+  height = 24,
+  color = "black",
+}: SVGProps<SVGElement>): JSX.Element {
+  return (
+    <svg
+      width={width}
+      height={height}
+      fill={color}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="4" cy="12" r="3">
+        <animate
+          id="a"
+          begin="0;b.end+0.25s"
+          attributeName="cy"
+          calcMode="spline"
+          dur="0.6s"
+          values="12;6;12"
+          keySplines=".33,.66,.66,1;.33,0,.66,.33"
+        />
+      </circle>
+      <circle cx="12" cy="12" r="3">
+        <animate
+          begin="a.begin+0.1s"
+          attributeName="cy"
+          calcMode="spline"
+          dur="0.6s"
+          values="12;6;12"
+          keySplines=".33,.66,.66,1;.33,0,.66,.33"
+        />
+      </circle>
+      <circle cx="20" cy="12" r="3">
+        <animate
+          id="b"
+          begin="a.begin+0.2s"
+          attributeName="cy"
+          calcMode="spline"
+          dur="0.6s"
+          values="12;6;12"
+          keySplines=".33,.66,.66,1;.33,0,.66,.33"
+        />
+      </circle>
+    </svg>
+  );
+}
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ReactComponent({ name }: { name: string }): JSX.Element {
-  return <div>{name}</div>;
-}
