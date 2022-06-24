affddf97f694357c7441fabcd5ce778fcc18e441
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-06-24 18:12:08 +0000

    feat: add ThreeDotScale component

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./components/ThreeDotsFade";
 export * from "./components/ThreeDotsMoving";
 export * from "./components/ThreeDotsRotate";
 export * from "./components/ThreeDotsScaleMiddle";
+export * from "./components/ThreeDotsScale";
